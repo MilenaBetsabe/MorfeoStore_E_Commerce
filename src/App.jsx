c54281ae3609793f070b3f663dcbef8c173f5afc
MilenaBetsabe <milenabetsabe.e@gmail.com>
2025-01-11 22:18:19 -0300

    Refactor: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      <NavBar />
-      <ItemListContainer greeting='Hola Mundo'/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<div><h1>Homepage</h1></div>} />
+          <Route path="/contacto" element={<div><h1>Contacto</h1></div>} />
+          <Route path="/adulto" element={<div><h1>Adulto</h1></div>} />
+        </Routes>
+      </BrowserRouter>
+      <ItemListContainer greeting='Hola Mundo' />
     </>
   )
 }
