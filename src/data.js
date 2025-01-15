c20cfda82b2349a83e09571f8b2a2def0cf21bc7
MilenaBetsabe <milenabetsabe.e@gmail.com>
2025-01-15 18:50:13 -0300

    ADD: create data of products

---
+++ src/data.js
@@ -0,0 +1,107 @@
+const products = [ //elementos harcodeados
+    {
+        "id": 1,
+        "name": "Alimento Excellent Gato Adulto",
+        "category":"adulto",
+        "price": 100.0,
+        "kg": 1.0,
+        "stock": 10,
+        "imageUrl": "https://cordoba.pluspet.com.ar/cdn/shop/files/alimento-purina-excellent-gato-adulto-01.jpg?v=1712604552&width=800"
+    },
+    {
+        "id": 2,
+        "name": "Alimento Agility Gato Adulto",
+        "category":"adulto",
+        "price": 150.0,
+        "kg": 1.5,
+        "stock": 5,
+        "imageUrl": "https://cordoba.pluspet.com.ar/cdn/shop/files/alimento-agility-gato-adulto-01.jpg?v=1735835555&width=800"
+    },
+    {
+        "id": 3,
+        "name": "Alimento Cat Chow Gato Adulto sabor Pescado y Pollo",
+        "category":"adulto",
+        "price": 200.0,
+        "kg": 2.0,
+        "stock": 20,
+        "imageUrl": "https://cordoba.pluspet.com.ar/cdn/shop/files/alimento-cat-chow-gato-adulto-pescado-y-pollo-01_f88961e1-05a5-4fe3-893f-e56c92627464.jpg?v=1702928626&width=1000"
+    },
+    {
+        "id": 4,
+        "name": "Alimento Old Prince Premium Gato Adulto",
+        "category":"adulto",
+        "price": 200.0,
+        "kg": 2.0,
+        "stock": 0,
+        "imageUrl": "https://cordoba.pluspet.com.ar/cdn/shop/files/alimento-old-prince-gato-premium-adulto-01_4bd45007-796a-4bec-b6af-07e3404fd3f2.jpg?v=1734035596&width=1000"
+    },
+    {
+        "id": 5,
+        "name": "Alimento Excellent Gatito Cachorro Kitten",
+        "category":"cachorro",
+        "price": 200.0,
+        "kg": 1.0,
+        "stock": 1,
+        "imageUrl": "https://cordoba.pluspet.com.ar/cdn/shop/files/alimento-purina-excellent-gato-cachorro-kitten-01.jpg?v=1712604279&width=1000"
+    },
+    {
+        "id": 6,
+        "name": "Alimento Pro Plan Gato Adulto edad 7+",
+        "category":"senior",
+        "price": 150.0,
+        "kg": 1.5,
+        "stock": 4,
+        "imageUrl": "https://cordoba.pluspet.com.ar/cdn/shop/files/alimento-gato-purina-pro-plan-adulto-senior-01.jpg?v=1703105183&width=1000"
+    },
+    {
+        "id": 7,
+        "name": "Cepillo Doble Grande con Puas Metálicas",
+        "category":"cepillos",
+        "price": 120.0,
+        "stock": 20,
+        "imageUrl": "https://cordoba.pluspet.com.ar/cdn/shop/products/CEPILLOGRANDECONPUASMETALICASWAHL.jpg?v=1622818859&width=400"
+    },
+    {
+        "id": 8,
+        "name": "Repelente para Perros y Gatos A Otra Parte Aplicación Directa 1lt",
+        "category":"repelentes",
+        "price": 1200.0,
+        "stock": 0,
+        "imageUrl": "https://cordoba.pluspet.com.ar/cdn/shop/files/repelente-a-otra-parte-01.jpg?v=1701785442&width=1000"
+    },
+    {
+        "id": 9,
+        "name": "Rascador Interactivo para gatos Nube con Pelota",
+        "category":"diversion",
+        "measure":"48x24x11cm",
+        "price": 100.0,
+        "stock": 10,
+        "imageUrl": "https://cordoba.pluspet.com.ar/cdn/shop/files/rascador-rascat-interactivo-nube-con-pelota-01.jpg?v=1728145109&width=1000"
+    },
+    {
+        "id": 10,
+        "name": "Caja de Arena Azul para Gatos",
+        "category":"accesorios",
+        "price": 150.0,
+        "stock": 5,
+        "imageUrl": "https://cordoba.pluspet.com.ar/cdn/shop/products/accesorios-sanremo-caja-arena-gato-azul-01.jpg?v=1706739429&width=1000"
+    },
+    {
+        "id": 11,
+        "name": "Pala Azul para Caja de Arena",
+        "category":"accesorios",
+        "price": 200.0,
+        "stock": 20,
+        "imageUrl": "https://cordoba.pluspet.com.ar/cdn/shop/products/accesorios-sanremo-pala-caja-arena-gato-azul-01.jpg?v=1706739451&width=1000"
+    },
+    {
+        "id": 12,
+        "name": "Alimento Agility Gatito Cachorro Kitten",
+        "category":"cachorro",
+        "price": 300.0,
+        "kg": 2.0,
+        "stock": 0,
+        "imageUrl": "https://cordoba.pluspet.com.ar/cdn/shop/files/alimento-agility-gato-kitten-cachorro-01.jpg?v=1735835555&width=1000"
+    }
+];
+export default products;
