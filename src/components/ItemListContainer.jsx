fe24ecd59e4ff78de7ab14c29bd51d42dfac0141
MilenaBetsabe <milenabetsabe.e@gmail.com>
2025-01-15 20:04:53 -0300

    ADD: Implementing hooks and methods

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,36 @@
-const ItemListContainer = ({greeting}) => {
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import getAllProducts, { getProductsByCategory } from '../getAsyncData.js'
+import { Container, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemList from "./ItemList";
+
+const ItemListContainer = ({ greeting }) => {
+
+    let { categoryId } = useParams();
+    const [products, setProducts] = useState([]);
+
+
+    useEffect(() => {
+        const asyncFunc = categoryId ? getProductsByCategory : getAllProducts
+        asyncFunc(categoryId)
+            .then(response => {
+                setProducts(response)
+            },
+            )
+            .catch(error => {
+                console.error('Error fetching products:', error);
+            })
+    }, [categoryId]);
 
     return (
         <>
-            <h2>{greeting}</h2> 
+            <Container>
+                <h1 className="my-4"> {greeting} </h1>
+                <Row>
+                    <ItemList products={products}></ItemList>
+                </Row>
+            </Container>
         </>
     )
 }
