7d701cb16bffe4188590179b72c9b5ef15e3e104
MilenaBetsabe <milenabetsabe.e@gmail.com>
2025-01-15 20:01:21 -0300

    REFACTOR: Changing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer'
+import ItemListContainer from './components/ItemListContainer';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import ItemDetailContainer from "./components/ItemDetailContainer"
 function App() {
 
   return (
@@ -10,12 +10,12 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route path="/" element={<div><h1>Homepage</h1></div>} />
-          <Route path="/contacto" element={<div><h1>Contacto</h1></div>} />
-          <Route path="/adulto" element={<div><h1>Adulto</h1></div>} />
+          <Route path="/" element={<ItemListContainer/>} />
+          <Route path="/category/:categoryId" element={<ItemListContainer />} />
+          <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="*" element={<div><h1>Error 404</h1></div>} />
         </Routes>
       </BrowserRouter>
-      <ItemListContainer greeting='Hola Mundo' />
     </>
   )
 }
